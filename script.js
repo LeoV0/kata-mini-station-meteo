f14cb657ddb202d52ffeb1222c21f1d6567932ee
leoV0 <leo.cigale@gmail.com>
2025-03-28 10:24:24 +0100

    nettoyage

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ async function getLocalisation(ville) {
     } else {
       affichageVille.innerHTML = `${data[0].address.city}`;
     }
-    maj.innerHTML = "";
+    maj.innerHTML = "Température actuelle";
     coordoneeGps.innerHTML = `Coordonnées GPS : ${data[0].lat} ${data[0].lon}`;
     const longitude = data[0].lon;
     const latitude = data[0].lat;
@@ -35,13 +35,11 @@ async function getLocalisation(ville) {
     maj.innerHTML = "Vérifier le nom de la ville";
   }
 }
-// getLocalisation(inputText.value);
 
 async function getMeteo(longitude, latitude) {
   const promise = await fetch(
     `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,precipitation,relative_humidity_2m`
   );
   const dataMeteo = await promise.json();
-  console.log(dataMeteo.current.temperature_2m);
   temperature.innerHTML = `${dataMeteo.current.temperature_2m} °C`;
 }
